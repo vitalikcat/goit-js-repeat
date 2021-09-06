a3fea68bb461963f7352e05aeb4d7cdd2d2e01e4
Vitalii Chepak <vitalik791@gmail.com>
2021-09-06 15:04:04 +0300

    added

---
+++ task01.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const NAME = "Генератор защитного поля";
+let price = 1000;
+
+console.log(`Выбран «${NAME}», цена за штуку ${price} кредитов'`);
+
+price = 2000;
+
+console.log(`Выбран «${NAME}», цена за штуку ${price} кредитов'`);
